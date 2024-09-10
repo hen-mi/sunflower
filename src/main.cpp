5902763c6abda71dce82b5fe282bc74911d889dd
hen-mi <hen.mizevski@gmail.com>
2024-09-10 16:13:04 -0300

    add readFromFile

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+namespace Sunflower 
+{
+    std::string readFromFile(const std::string& filePath) 
+    {
+        std::ifstream       file(filePath);
+        std::stringstream   buffer;
+
+
+        buffer << file.rdbuf();
+        return buffer.str();
+    }
+}
 
 int main(int argc, char** argv)
 {
-
+    std::string filePath = "examples/test.sun";
+    std::cout << Sunflower::readFromFile(filePath);
 }
