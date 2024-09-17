1aaf5225dfa81f7ff5695fbc13441369dc5e7c5d
hen-mi <hen.mizevski@gmail.com>
2024-09-17 18:59:14 -0300

    Token fix

--- src/Token.h
+++ src/Token.h
@@ -1,15 +1,15 @@
 #pragma once
-
 #include <string>
+
 enum class TokenType 
 {
 	
 };
 
-struct Token 
+struct Token final
 {
 	TokenType		    t;
 	std::string		value;
 	uint32_t		 line;
 
-} using Token;
+};
