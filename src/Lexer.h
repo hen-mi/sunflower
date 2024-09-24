efe88f904230c8f4291ce0d8fd10de4b54afc296
hen-mi <hen.mizevski@gmail.com>
2024-09-24 18:50:32 -0300

    small change

--- src/Lexer.h
+++ src/Lexer.h
@@ -1,7 +1,5 @@
 #pragma once
 #include <sunflower.h>
-#include <Token.h>
-
 
 namespace Sunflower
 {
--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@ int main(int argc, char** argv)
    
     Sunflower::Lexer l(Sunflower::readFromFile(filePath));
 
-
     Sunflower::printSymbolsTable();
    
 }
