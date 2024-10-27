1d679c8317096a7c701b7eebd9b463598ed55adc
hen-mi <hen.mizevski@gmail.com>
2024-10-27 20:33:14 -0300

    Bug fix

--- src/AST.h
+++ src/AST.h
@@ -2,18 +2,22 @@
 #include <Token.h>
 #include <memory>
 #include <any>
-struct Visitor 
+
+class BinaryExpr;
+class UnaryExpr;
+class GroupingExpr;
+class Literal;
+
+class Visitor
 {
-	virtual void visit(BinaryExpr &node) = 0;
+public:
+	virtual void visit(BinaryExpr& node) = 0;
 	virtual void visit(UnaryExpr& node) = 0;
 	virtual void visit(GroupingExpr& node) = 0;
 	virtual void visit(Literal& node) = 0;
 
 	virtual ~Visitor() = default;
 };
-
-
-
 class Expr 
 {
 public:
@@ -28,10 +32,10 @@ class BinaryExpr : public Expr
 {
 public:
 
-	BinaryExpr(Expr left, Token op, Expr right) : 
-				mLeft(&left), mRight(&right), mOp(op) {}
+	BinaryExpr(std::unique_ptr<Expr> left, Token op, std::unique_ptr<Expr> right) :
+				mLeft(std::move(left)), mRight(std::move(right)), mOp(op) {}
 
-	virtual void accept(Visitor& v) override { v.visit(*this); }
+	void accept(Visitor& v) override { v.visit(*this); }
 
 private:
 	const std::unique_ptr<Expr> mLeft;
@@ -43,9 +47,9 @@ class UnaryExpr : public Expr
 {
 public:
 
-	UnaryExpr(Token op, Expr right) : mRight(&right), mOp(op) {}
+	UnaryExpr(Token op, std::unique_ptr<Expr> right) : mRight(std::move(right)), mOp(op) {}
 
-	virtual void accept(Visitor& v) override { v.visit(*this); }
+	void accept(Visitor& v) override { v.visit(*this); }
 
 private:
 	const std::unique_ptr<Expr> mRight;
@@ -56,9 +60,9 @@ private:
 class GroupingExpr : public Expr 
 {
 public:
-	GroupingExpr(Expr expression) : mExpr(&expression) {}
+	GroupingExpr(std::unique_ptr<Expr> expression) : mExpr(std::move(expression)) {}
 
-	virtual void accept(Visitor& v) override { v.visit(*this); }
+	void accept(Visitor& v) override { v.visit(*this); }
 
 private:
 	const std::unique_ptr<Expr> mExpr;
@@ -71,7 +75,7 @@ class Literal : public Expr
 public:
 
 	Literal(std::any v) : mValue(v) {}
-	virtual void accept(Visitor& v) override { v.visit(*this); }
+	void accept(Visitor& v) override { v.visit(*this); }
 
 private:
 	const std::any mValue;
