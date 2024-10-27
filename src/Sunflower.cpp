1fc800fc53e172baa5d67fc934d9740087463818
hen-mi <hen.mizevski@gmail.com>
2024-10-27 18:05:05 -0300

    Basic error handling

--- src/Sunflower.cpp
+++ src/Sunflower.cpp
@@ -1,6 +1,21 @@
 #include <sunflower.h>
 namespace Sunflower 
 {
+    void Sunflower::report(int line, const std::string& where, const std::string& message)  
+    {
+        std::cerr << "[line: " << line << " ]" << "Error: " << where << " " << message << "\n";
+    }
+
+    void Sunflower::error(Token token, const std::string& message) 
+    {
+        if(token.tokentype == TokenType::_EOF) 
+        {
+            report(token.line, "at end", message);
+
+        } else {
+            report(token.line, token.lexema + "'", message);
+        }
+    }
 
     std::string Sunflower::readFromFile(const std::string& filePath)
     {
--- src/Sunflower.h
+++ src/Sunflower.h
@@ -5,9 +5,12 @@
 #include <string>
 #include <vector>
 #include <Token.h>
-
+#include <stack>
 namespace Sunflower
 {
+	void error(Token token, const std::string& message);
+	void report(int line, const std::string& where, const std::string& message);
+
 	std::string readFromFile(const std::string& filePath);
 	void printSymbolsTable();
 
