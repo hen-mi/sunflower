e54a33b0930c2b3955419f03b55aa8eefb8b9337
hen-mi <hen.mizevski@gmail.com>
2024-10-27 18:35:18 -0300

    Add Expr, BinaryExpr, GroupingExpr, UnaryExpr and Literal nodes

--- src/AST.h
+++ src/AST.h
@@ -1,10 +1,13 @@
 //Abstract Syntax Tree
 #include <Token.h>
 #include <memory>
-
+#include <any>
 struct Visitor 
 {
 	virtual void visit(BinaryExpr &node) = 0;
+	virtual void visit(UnaryExpr& node) = 0;
+	virtual void visit(GroupingExpr& node) = 0;
+	virtual void visit(Literal& node) = 0;
 
 	virtual ~Visitor() = default;
 };
@@ -13,11 +16,12 @@ struct Visitor
 
 class Expr 
 {
-	public:
+public:
 	virtual void accept(Visitor	&v) = 0;
 	virtual ~Expr() = default;
 
 };
+
 
 
 class BinaryExpr : public Expr 
@@ -25,25 +29,53 @@ class BinaryExpr : public Expr
 public:
 
 	BinaryExpr(Expr left, Token op, Expr right) : 
-				mLeft(&left), mRight(&right), m_op(op) {}
+				mLeft(&left), mRight(&right), mOp(op) {}
 
 	virtual void accept(Visitor& v) override { v.visit(*this); }
 
 private:
 	const std::unique_ptr<Expr> mLeft;
 	const std::unique_ptr<Expr> mRight;
-	const Token m_op;
+	const Token mOp;
 };
-
 
 class UnaryExpr : public Expr 
 {
+public:
+
+	UnaryExpr(Token op, Expr right) : mRight(&right), mOp(op) {}
+
+	virtual void accept(Visitor& v) override { v.visit(*this); }
+
+private:
+	const std::unique_ptr<Expr> mRight;
+	const Token mOp;
 };
 
 
-class Grouping : public Expr {};
+class GroupingExpr : public Expr 
+{
+public:
+	GroupingExpr(Expr expression) : mExpr(&expression) {}
+
+	virtual void accept(Visitor& v) override { v.visit(*this); }
+
+private:
+	const std::unique_ptr<Expr> mExpr;
+
+};
 
 
-class NumericLiteral : public Expr {};
+class Literal : public Expr 
+{
+public:
+
+	Literal(std::any v) : mValue(v) {}
+	virtual void accept(Visitor& v) override { v.visit(*this); }
+
+private:
+	const std::any mValue;
+
+};
 
 
